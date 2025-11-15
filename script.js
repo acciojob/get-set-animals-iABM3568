ed0b425047f882f22f3011e72cb10aa7d0628786
Abhishek Mishra <iabm3568@gmail.com>
2025-11-15 12:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species) {
+    // Store the species as a private property
+    this._species = species;
+  }
+  
+  // Getter for species
+  get species() {
+    return this._species;
+  }
+  
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species) {
+    // Call the parent Animal constructor
+    super(species);
+  }
+  
+  // Dog-specific method
+  bark() {
+    console.log("woof");
+  }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species) {
+    // Call the parent Animal constructor
+    super(species);
+  }
+  
+  // Cat-specific method
+  purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
